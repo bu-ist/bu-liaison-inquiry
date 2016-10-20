976e3f9eda9d91788a6ae496a23c9e82d37e831d
jdub233 <tamlin@mac.com>
2016-10-20 17:17:01 -0400

    add basic form handling

--- bu-liaison-inquiry.php
+++ bu-liaison-inquiry.php
@@ -60,8 +60,77 @@ add_shortcode( 'liaison_inquiry_form', 'liaison_inquiry_form' );
 
 
 function handle_liaison_inquiry() {
-	var_export( $_POST );
-	exit();
+
+	// Check that it looks like the right form.
+	if ( 'liaison_inquiry_form' != $_POST['form_submit'] ) {
+		//now what?
+		echo 'wrong form';
+		return;
+	}
+
+	unset( $_POST['form_submit'] );
+
+	// Necessary to get the API key from the options, can't expose the key by passing it through the form.
+	$options = get_option( 'bu_liaison_inquiry_options' );
+	// Check for a valid API key value.
+	if ( !isset( $options['APIKey'] ) ) {
+		echo 'no API key';
+		return;
+	}
+
+	$_POST['IQS-API-KEY'] = $options['APIKey'];
+
+
+	// Straight from API example
+	$phone_fields = $_POST['phone_fields'];
+	$phone_fields = explode(',', $phone_fields);
+	unset($_POST['phone_fields']);
+
+
+	$post_vars = array();
+	foreach ($_POST as $k => $v) {
+		if (in_array($k, $phone_fields)) {
+			$v = preg_replace('/[^0-9]/', '', $v);
+			$v = '%2B1' . $v;		// append +1 for US, but + needs to be %2B for posting
+		}
+		// if this checkbox field is set then it was checked
+		if (stripos($k, '-text-opt-in') !== false) {
+			$v = '1';
+		}
+		//$post_vars[] = $k . '=' . $v;
+	}
+	//$post_vars = implode('&', $post_vars);
+
+	//shim
+	$post_vars = $_POST;
+
+	//End API Example
+
+
+	$post_args = array( 'body' => $post_vars );
+
+	$remote_submit = wp_remote_post( SUBMIT_URL, $post_args );
+
+	// Check response status: if successfull then redirect to the new supplied purl.
+
+	$resp = json_decode( $remote_submit['body'] );
+
+	//from spectrum
+	$return = array();
+	$return['status'] = 0;
+	
+	$return['status'] = (isset($resp->status) && $resp->status == 'success') ? 1 : 0;
+	$return['response'] = (isset($resp->message)) ? $resp->message : 'Something bad happened, please refresh the page and try again.';
+	$return['data'] = (isset($resp->data)) ? $resp->data : '';
+	//end spectrum
+
+
+	//echo json_encode($return);
+	$redirect_url = urldecode( $return['data'] );
+
+	echo esc_html( $return['response'] );
+	echo "<script>window.location.href = '" . esc_url( $redirect_url ) . "';</script>";
+
 }
 
 add_action( 'admin_post_nopriv_liaison_inquiry', 'handle_liaison_inquiry' );
