2fb61db24543c27abb9e46b9517f5a70db95a676
Anton Kachurin <kachurinanton@gmail.com>
2017-08-07 17:06:10 -0400

    Lint main script: tabs, docstrings

--- bu-liaison-inquiry.php
+++ bu-liaison-inquiry.php
@@ -1,79 +1,84 @@
 <?php
+/**
 
-/*
-Plugin Name: BU Liaison Inquiry
-Plugin URI: http://developer.bu.edu
-Author: Boston University IS&T (Jonathan Williams)
-Author URI: http://developer.bu.edu
-Description: Provide a form to send data to the Liaison SpectrumEMP API
-Version: 0.6
-*/
+ * Plugin Name: BU Liaison Inquiry
+ * Plugin URI: http://developer.bu.edu
+ * Author: Boston University IS&T (Jonathan Williams)
+ * Author URI: http://developer.bu.edu
+ * Description: Provide a form to send data to the Liaison SpectrumEMP API
+ * Version: 0.6
+ *
+ * @package BULiaisonInquiry
+ */
 
+/**
+ * Composer autoload
+ */
 require __DIR__ . '/vendor/autoload.php';
 
 use BULiaisonInquiry\Plugin;
 use BULiaisonInquiry\SpectrumAPI;
 
 // Instantiate plugin (only once).
-if (! isset($GLOBALS['bu_liaison_inquiry'])) {
-  // Get API key and Client ID from option settings.
-  $options = get_option( 'bu_liaison_inquiry_options' );
-  $api_key = $options['APIKey'];
-  $client_id = $options['ClientID'];
+if ( ! isset( $GLOBALS['bu_liaison_inquiry'] ) ) {
+	// Get API key and Client ID from option settings.
+	$options = get_option( 'bu_liaison_inquiry_options' );
+	$api_key = $options['APIKey'];
+	$client_id = $options['ClientID'];
 
-  $GLOBALS['bu_liaison_inquiry'] = new Plugin(new SpectrumAPI($api_key, $client_id));
+	$GLOBALS['bu_liaison_inquiry'] = new Plugin( new SpectrumAPI( $api_key, $client_id ) );
 }
 
 // Register js form validation scripts so that they may be enqueued
 // by the shortcode handler.
 wp_register_script(
-    'jquery-ui',
-    plugin_dir_url(__FILE__) . 'assets/js/jquery/jquery-ui.min.js',
-    array( 'jquery' )
+	'jquery-ui',
+	plugin_dir_url( __FILE__ ) . 'assets/js/jquery/jquery-ui.min.js',
+	array( 'jquery' )
 );
 
 wp_register_script(
-    'jquery-masked',
-    plugin_dir_url(__FILE__) . 'assets/js/jquery/jquery-masked.js',
-    array( 'jquery' )
+	'jquery-masked',
+	plugin_dir_url( __FILE__ ) . 'assets/js/jquery/jquery-masked.js',
+	array( 'jquery' )
 );
 
 wp_register_script(
-    'jquery-pubsub',
-    plugin_dir_url(__FILE__) . 'assets/js/jquery/jquery-pubsub.js',
-    array( 'jquery' )
+	'jquery-pubsub',
+	plugin_dir_url( __FILE__ ) . 'assets/js/jquery/jquery-pubsub.js',
+	array( 'jquery' )
 );
 
 wp_register_script(
-    'iqs-validate',
-    plugin_dir_url(__FILE__) . 'assets/js/iqs/validate.js',
-    array( 'jquery' )
+	'iqs-validate',
+	plugin_dir_url( __FILE__ ) . 'assets/js/iqs/validate.js',
+	array( 'jquery' )
 );
 
 wp_register_script(
-    'field_rules_form_library',
-    plugin_dir_url(__FILE__) . 'assets/js/field_rules_form_library.js',
-    array( 'jquery' )
+	'field_rules_form_library',
+	plugin_dir_url( __FILE__ ) . 'assets/js/field_rules_form_library.js',
+	array( 'jquery' )
 );
 
 wp_register_script(
-    'field_rules_handler',
-    plugin_dir_url(__FILE__) . 'assets/js/field_rules_handler.js',
-    array( 'jquery' )
+	'field_rules_handler',
+	plugin_dir_url( __FILE__ ) . 'assets/js/field_rules_handler.js',
+	array( 'jquery' )
 );
 
 wp_register_script(
-    'bu-liaison-main',
-    plugin_dir_url(__FILE__) . 'assets/js/main.js',
-    array( 'jquery' )
+	'bu-liaison-main',
+	plugin_dir_url( __FILE__ ) . 'assets/js/main.js',
+	array( 'jquery' )
 );
 
 wp_register_style(
-    'liason-form-style',
-    plugin_dir_url(__FILE__) . 'assets/css/form-style.css'
+	'liason-form-style',
+	plugin_dir_url( __FILE__ ) . 'assets/css/form-style.css'
 );
 
 wp_register_style(
-    'jquery-ui-css',
-    plugin_dir_url(__FILE__) . 'assets/js/jquery/jquery-ui.min.css'
+	'jquery-ui-css',
+	plugin_dir_url( __FILE__ ) . 'assets/js/jquery/jquery-ui.min.css'
 );
