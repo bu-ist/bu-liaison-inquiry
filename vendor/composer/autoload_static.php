2183d46e35242e2be42b35a3dedad3755d6baf3a
Anton Kachurin <kachurinanton@gmail.com>
2017-08-18 15:40:06 -0400

    Rebuild autoload classmap

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,10 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInit83a43bff8caa94ded7a01ab7f29fe114
 {
     public static $classMap = array (
-        'BU_Liaison_Inquiry\\Admin' => __DIR__ . '/../..' . '/includes/class-admin.php',
-        'BU_Liaison_Inquiry\\Mock_Spectrum_API' => __DIR__ . '/../..' . '/includes/class-mock-spectrum-api.php',
-        'BU_Liaison_Inquiry\\Plugin' => __DIR__ . '/../..' . '/includes/class-plugin.php',
-        'BU_Liaison_Inquiry\\Spectrum_API' => __DIR__ . '/../..' . '/includes/class-spectrum-api.php',
+        'BU\\Plugins\\Liaison_Inquiry\\Admin' => __DIR__ . '/../..' . '/includes/class-admin.php',
+        'BU\\Plugins\\Liaison_Inquiry\\Mock_Spectrum_API' => __DIR__ . '/../..' . '/includes/class-mock-spectrum-api.php',
+        'BU\\Plugins\\Liaison_Inquiry\\Plugin' => __DIR__ . '/../..' . '/includes/class-plugin.php',
+        'BU\\Plugins\\Liaison_Inquiry\\Spectrum_API' => __DIR__ . '/../..' . '/includes/class-spectrum-api.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
