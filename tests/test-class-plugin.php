dd9bd5f3ea94764455417f3dce46b509901760e5
Anton Kachurin <kachurinanton@gmail.com>
2017-08-21 11:27:40 -0400

    Simplify tests

--- tests/test-class-plugin.php
+++ tests/test-class-plugin.php
@@ -3,8 +3,7 @@
 class BU_Liaison_Inquiry_Test_Plugin extends WP_UnitTestCase {
 
 	public function setUp() {
-		$this->fixture_api = new BU\Plugins\Liaison_Inquiry\Mock_Spectrum_API('', '');
-		$this->real_plugin_instance = new BU\Plugins\Liaison_Inquiry\Plugin($this->fixture_api);
+		$this->spectrum = $this->createMock(BU\Plugins\Liaison_Inquiry\Spectrum_API::class);
 	}
 
 	/**
@@ -12,27 +11,19 @@ class BU_Liaison_Inquiry_Test_Plugin extends WP_UnitTestCase {
 	 */
 	public function test_liaison_inquiry_form() {
 		$shortcode_attributes = null;
-		$requirements = $this->fixture_api->get_requirements();
-		$form_definition = $this->real_plugin_instance->minify_form_definition(
-			$requirements,
-			$shortcode_attributes
-		);
+		$requirements = 'requirements coming from api';
+		$form_definition = 'form definition';
 		$form_html = 'html response';
 
-		$api = $this->getMockBuilder(BU\Plugins\Liaison_Inquiry\Mock_Spectrum_API::class)
-								->setConstructorArgs(['', ''])
-								->setMethods(['get_requirements'])
-								->getMock();
-
 		$plugin = $this->getMockBuilder(BU\Plugins\Liaison_Inquiry\Plugin::class)
-									 ->setConstructorArgs([$api])
+									 ->setConstructorArgs([$this->spectrum])
 									 ->setMethods(['minify_form_definition', 'get_form_html'])
 									 ->getMock();
 
 		// Spectrum_API::get_requirements is called
-		$api->expects($this->once())
-				->method('get_requirements')
-				->willReturn($requirements);
+		$this->spectrum->expects($this->once())
+									 ->method('get_requirements')
+									 ->willReturn($requirements);
 
 		// Plugin::minify_form_definition is called with proper arguments
 		$plugin->expects($this->once())
@@ -57,19 +48,14 @@ class BU_Liaison_Inquiry_Test_Plugin extends WP_UnitTestCase {
 	{
 		$exception_message = 'error in api response';
 
-		$api = $this->getMockBuilder(BU\Plugins\Liaison_Inquiry\Mock_Spectrum_API::class)
-								->setConstructorArgs(['', ''])
-								->setMethods(['get_requirements'])
-								->getMock();
-
 		$plugin = $this->getMockBuilder(BU\Plugins\Liaison_Inquiry\Plugin::class)
-									 ->setConstructorArgs([$api])
+									 ->setConstructorArgs([$this->spectrum])
 									 ->setMethods(null)
 									 ->getMock();
 
 		// Spectrum_API::get_requirements throws the exception.
-		$api->method('get_requirements')
-				->will($this->throwException(new \Exception($exception_message)));
+		$this->spectrum->method('get_requirements')
+									 ->will($this->throwException(new \Exception($exception_message)));
 
 		// Method returns the value of Exception::getMessage
 		$this->assertEquals($exception_message, $plugin->liaison_inquiry_form(null));
