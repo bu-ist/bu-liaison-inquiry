47daf722715c1852a11e0bd2b5bc383d4009a4ac
Anton Kachurin <kachurinanton@gmail.com>
2017-08-23 14:01:30 -0400

    Exclude Sample_Spectrum_API class from testing

--- includes/class-sample-spectrum-api.php
+++ includes/class-sample-spectrum-api.php
@@ -12,6 +12,8 @@ namespace BU\Plugins\Liaison_Inquiry;
  *
  * Primarily used during development to avoid sending any requests to the remote server.
  * Used in place of SpectrumAPI class.
+ *
+ * @codeCoverageIgnore
  */
 class Sample_Spectrum_API {
 
