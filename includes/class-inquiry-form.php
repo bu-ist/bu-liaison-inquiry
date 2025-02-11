5d7431ea8507de57ec0f3fda886e81691ed9435d
Ben Fenster <bfenster@bu.edu>
2025-02-11 12:00:47 -0700

    Do not include UTM fields unless the UTM parameters are actually present in the original URL

--- includes/class-inquiry-form.php
+++ includes/class-inquiry-form.php
@@ -79,12 +79,14 @@ class Inquiry_Form {
 		// but those fields are not normally in the form definition. We'll
 		// add them ourselves.
 		foreach( Settings::list_utm_values() as $field_name => $id ) {
-			$field = new \stdClass();
-			$field->hidden = 1;
-			$field->id = $id;
-			$field->hidden_value = $_GET[ $field_name ] ?? '';
+			if ( isset( $_GET[ $field_name ] ) && trim( $_GET[ $field_name ] ) !== '' ) {
+				$field = new \stdClass();
+				$field->hidden = 1;
+				$field->id = $id;
+				$field->hidden_value = $_GET[ $field_name ] ?? '';
 
-			$form_definition->sections[ count( $form_definition->sections ) - 1 ]->fields[] = $field;
+				$form_definition->sections[ count( $form_definition->sections ) - 1 ]->fields[] = $field;
+			}
 		}
 
 		$form_definition = $this->autofill_fields(
