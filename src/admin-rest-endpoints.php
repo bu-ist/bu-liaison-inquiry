efe6baaf0c10b2ff15dd75611112e4ec02f0ece9
Jonathan Williams <tamlin@mac.com>
2025-06-04 17:21:51 -0400

    simplify data handling
    Simplifies credential management by directly updating options
    with provided parameters.

--- src/admin-rest-endpoints.php
+++ src/admin-rest-endpoints.php
@@ -25,14 +25,14 @@ function register_rest_routes() {
 			array(
 				'methods'             => \WP_REST_Server::READABLE,
 				'callback'            => __NAMESPACE__ . '\get_credentials',
-				'permission_callback' => function() {
+				'permission_callback' => function () {
 					return current_user_can( 'manage_options' );
 				},
 			),
 			array(
 				'methods'             => \WP_REST_Server::CREATABLE,
 				'callback'            => __NAMESPACE__ . '\create_credential',
-				'permission_callback' => function() {
+				'permission_callback' => function () {
 					return current_user_can( 'manage_options' );
 				},
 			),
@@ -43,24 +43,17 @@ function register_rest_routes() {
 /**
  * Get all organization credentials
  *
- * @param \WP_REST_Request $request The request object.
  * @return \WP_REST_Response|\WP_Error
  */
-function get_credentials( $request ) {
+function get_credentials() {
 	$options = get_option( 'bu_liaison_inquiry_options', array() );
-	
-	// Format the response.
-	/*
-    $credentials = array();
-	if ( ! empty( $options['org_credentials'] ) ) {
-		$credentials = $options['org_credentials'];
-	}
-    */
 	return rest_ensure_response( $options );
 }
 
 /**
  * Create a new organization credential
+ * 
+ * Not tested yet
  *
  * @param \WP_REST_Request $request The request object.
  * @return \WP_REST_Response|\WP_Error
@@ -68,37 +61,11 @@ function get_credentials( $request ) {
 function create_credential( $request ) {
 	$params = $request->get_json_params();
 
-	// Validate required fields.
-	$required = array( 'org_key', 'api_key', 'client_id' );
-	foreach ( $required as $field ) {
-		if ( empty( $params[ $field ] ) ) {
-			return new \WP_Error(
-				'missing_required_field',
-				sprintf( __( 'Missing required field: %s', 'bu-liaison-inquiry' ), $field ),
-				array( 'status' => 400 )
-			);
-		}
-	}
-
-	// Get existing options.
-	$options = get_option( 'bu_liaison_inquiry_options', array() );
-	if ( empty( $options['org_credentials'] ) ) {
-		$options['org_credentials'] = array();
-	}
-
-	// Add new credential.
-	$options['org_credentials'][ $params['org_key'] ] = array(
-		'org_key'   => sanitize_text_field( $params['org_key'] ),
-		'api_key'   => sanitize_text_field( $params['api_key'] ),
-		'client_id' => sanitize_text_field( $params['client_id'] ),
-	);
-
 	// Update options.
-	update_option( 'bu_liaison_inquiry_options', $options );
+	update_option( 'bu_liaison_inquiry_options', $params );
 
-	return rest_ensure_response( $options['org_credentials'][ $params['org_key'] ] );
+	return rest_ensure_response( $params );
 }
 
 // Register routes.
 add_action( 'rest_api_init', __NAMESPACE__ . '\register_rest_routes' );
-
