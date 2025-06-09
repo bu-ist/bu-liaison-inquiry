a3283fc08891cbde9d3c010711e3e3c120979742
Jonathan Williams <tamlin@mac.com>
2025-06-09 11:07:01 -0400

    remove unused

--- src/components/CredentialModal.js
+++ src/components/CredentialModal.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useState, useEffect } from '@wordpress/element';
+import { useState } from '@wordpress/element';
 import { 
     Modal,
     TextControl, 
